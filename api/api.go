766ca6a79a73d1be7f7320488530ffe0e001d7eb
Junaid Rahim <junaidrahim8d@gmail.com>
2022-08-21 21:41:08 +0530

    feat: add parser and renderer (#3)

--- api/api.go
+++ api/api.go
@@ -1,1 +1,30 @@
 package api
+
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/prodocsdev/prodocs/config"
+)
+
+type ProdocsHTTPServer struct {
+	server *echo.Echo
+	port   string
+}
+
+// NewProdocsHTTPServer is
+func NewProdocsHTTPServer(config *config.ProdocsConfig) (*ProdocsHTTPServer, error) {
+	s := ProdocsHTTPServer{
+		server: echo.New(),
+		port:   config.Port,
+	}
+	return &s, nil
+}
+
+func (p *ProdocsHTTPServer) Start() {
+	p.setupRoutes()
+	p.server.Logger.Fatal(p.server.Start(fmt.Sprintf(":%v", p.port)))
+}
+
+func (p *ProdocsHTTPServer) setupRoutes() {
+
+}
