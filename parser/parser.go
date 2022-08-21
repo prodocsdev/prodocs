07978c4c79286766a07544f8e336c1c8de7ff1cc
Junaid Rahim <junaidrahim8d@gmail.com>
2022-08-21 21:02:21 +0530

    feat: add parser and renderer

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,27 @@
 package parser
+
+import "os"
+
+// MarkdownParser is the component that will read markdown files and find out
+// template and other plugin references
+type MarkdownParser struct{}
+
+type ProdocsParsedMarkdown struct {
+	Data       []byte
+	OutputPath string
+}
+
+func NewMarkdownParser() (*MarkdownParser, error) {
+	return &MarkdownParser{}, nil
+}
+
+func (mp *MarkdownParser) Parse(path string) (*ProdocsParsedMarkdown, error) {
+	mdData, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ProdocsParsedMarkdown{
+		Data: mdData,
+	}, nil
+}
--- renderer/renderer.go
+++ renderer/renderer.go
@@ -1,1 +1,32 @@
 package renderer
+
+import (
+	"fmt"
+	"github.com/gomarkdown/markdown"
+	mdparser "github.com/gomarkdown/markdown/parser"
+	parser "github.com/prodocsdev/prodocs/parser"
+	"os"
+)
+
+type Renderer struct {
+	outputRoot string
+}
+
+func NewRenderer(outputRoot string) (*Renderer, error) {
+	return &Renderer{
+		outputRoot: outputRoot,
+	}, nil
+}
+
+func (r *Renderer) Render(parsedMd *parser.ProdocsParsedMarkdown) error {
+	extensions := mdparser.CommonExtensions | mdparser.AutoHeadingIDs
+	extendedMarkdownParser := mdparser.NewWithExtensions(extensions)
+
+	html := markdown.ToHTML(parsedMd.Data, extendedMarkdownParser, nil)
+	err := os.WriteFile(fmt.Sprintf("%v/%v", r.outputRoot, parsedMd.OutputPath), html, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
